fc50c017df7d21d1f53d8765df5739a274b9edd7
nrednav <nrednav@gmail.com>
2020-04-06 20:45:48 +0400

    Add solution to problem 3

--- src/3.cpp
+++ src/3.cpp
@@ -2,15 +2,49 @@
 
 #include "Main.h"
 
-vector<int> getPrimeFactors(int n, vector<int> &prime_factors);
+bool is_prime(int n);
+void get_prime_factors(long long &n, vector<int> &prime_factors);
+
+vector<int> primes;
 
 int main() {
 
-  int n = 6'008'514'751'43;
+  long long n = 600851475143;
+
   vector<int> prime_factors;
-  getPrimeFactors(n, prime_factors);
+  get_prime_factors(n, prime_factors);
+
+  for (int factor: prime_factors) {
+    cout << factor << " ";
+  }
 
   return 0;
 }
 
-vector<int> getPrimeFactors(int n, vector<int> &prime_factors) {}
+bool is_prime(int n) {
+  for (int prime: primes) {
+    if (prime > sqrt(n))
+      break;
+
+    if (n % prime == 0)
+      return false;
+  }
+
+  primes.push_back(n);
+  return true;
+}
+
+void get_prime_factors(long long &n, vector<int> &prime_factors) {
+  int divisor = 2;
+
+  while (n > 1) {
+    if (is_prime(divisor)) {
+      if (n % divisor == 0) {
+        n /= divisor;
+        prime_factors.push_back(divisor);
+      }
+      else divisor++;
+    }
+    else divisor++;
+  }
+}
